58072801e7fddb69b4d794c517b4de9badf88e19
ayushijadhav2006 <124800578+ayushijadhav2006@users.noreply.github.com>
2026-03-01 01:31:35 +0530

    setting done

--- thikana-web/app/(dashboard)/(with-recommendations)/profile/layout.jsx
+++ thikana-web/app/(dashboard)/(with-recommendations)/profile/layout.jsx
@@ -1,13 +1,14 @@
+import Sidebar from "@/components/Sidebar";
 import React from "react";
 
 const Layout = ({ children }) => {
-    return (
-        <div className="flex items-center justify-center w-full">
-            <div className="max-w-[1400px] w-full mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="w-full">{children}</div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-center w-full">
+      <div className="max-w-[1400px] w-full flex justify-center gap-6 px-2 md:px-4">
+        <div className="w-full">{children}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
